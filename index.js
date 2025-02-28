e099548a78356e81e14af83ed3250ffff71081ab
Ankush Kumar <singhankush835@gmail.com>
2025-03-01 03:17:48 +0530

    added core

--- index.js
+++ index.js
@@ -17,12 +17,24 @@ const app = express();
 // }));
 
 app.use(cors({
-  origin: '*', 
-  credentials: true,
+  origin: "https://amazon-clone-frontend-neon.vercel.app", // Specific frontend origin
+  credentials: true, // Allow credentials (cookies, tokens)
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Required headers
 }));
+
 
 app.use(bodyParser.json());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://amazon-clone-frontend-neon.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 // Connect to the database
 connectDB();
