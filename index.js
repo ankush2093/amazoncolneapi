65230967ae33db850362bba4978e5636f1c3d1b1
Ankush Kumar <singhankush835@gmail.com>
2025-03-02 22:18:17 +0530

    added new localhost

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://amazon-clone-frontend-neon.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://amazon-clone-frontend-neon.vercel.app", "http://localhost:3000/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
